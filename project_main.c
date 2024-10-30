6f60c4dfa5f49632a91205b2266a857c1ef7ac99
Janne Paksuniemi <jannepasp@gmail.com>
2024-10-30 10:55:37 +0200

    Initial commit

---
+++ project_main.c
@@ -0,0 +1,103 @@
+/* C Standard library */
+#include <stdio.h>
+
+/* XDCtools files */
+#include <xdc/std.h>
+#include <xdc/runtime/System.h>
+
+/* BIOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/drivers/PIN.h>
+#include <ti/drivers/pin/PINCC26XX.h>
+#include <ti/drivers/I2C.h>
+#include <ti/drivers/Power.h>
+#include <ti/drivers/power/PowerCC26XX.h>
+#include <ti/drivers/UART.h>
+
+/* Board Header files */
+#include "Board.h"
+#include "sensors/opt3001.h"
+
+/* Task */
+#define STACKSIZE 2048
+Char sensorTaskStack[STACKSIZE];
+Char uartTaskStack[STACKSIZE];
+
+enum state { WAITING=1, DATA_READY };
+enum state programState = WAITING;
+
+void buttonFxn(PIN_Handle handle, PIN_Id pinId) {
+
+}
+
+/* Task Functions */
+Void uartTaskFxn(UArg arg0, UArg arg1) {
+
+
+    while (1) {
+        // Just for sanity check for exercise, you can comment this out
+        System_printf("uartTask\n");
+        System_flush();
+
+        // Once per second, you can modify this
+        Task_sleep(1000000 / Clock_tickPeriod);
+    }
+}
+
+Void sensorTaskFxn(UArg arg0, UArg arg1) {
+
+    I2C_Handle      i2c;
+    I2C_Params      i2cParams;
+
+    while (1) {
+
+        // Just for sanity check for exercise, you can comment this out
+        System_printf("sensorTask\n");
+        System_flush();
+
+        // Once per second, you can modify this
+        Task_sleep(1000000 / Clock_tickPeriod);
+    }
+}
+
+Int main(void) {
+
+    // Task variables
+    Task_Handle sensorTaskHandle;
+    Task_Params sensorTaskParams;
+    Task_Handle uartTaskHandle;
+    Task_Params uartTaskParams;
+
+    // Initialize board
+    Board_initGeneral();
+
+    /* Task */
+    Task_Params_init(&sensorTaskParams);
+    sensorTaskParams.stackSize = STACKSIZE;
+    sensorTaskParams.stack = &sensorTaskStack;
+    sensorTaskParams.priority=2;
+    sensorTaskHandle = Task_create(sensorTaskFxn, &sensorTaskParams, NULL);
+    if (sensorTaskHandle == NULL) {
+        System_abort("Task create failed!");
+    }
+
+    Task_Params_init(&uartTaskParams);
+    uartTaskParams.stackSize = STACKSIZE;
+    uartTaskParams.stack = &uartTaskStack;
+    uartTaskParams.priority=2;
+    uartTaskHandle = Task_create(uartTaskFxn, &uartTaskParams, NULL);
+    if (uartTaskHandle == NULL) {
+        System_abort("Task create failed!");
+    }
+
+    /* Sanity check */
+    System_printf("Hello world!\n");
+    System_flush();
+
+    /* Start BIOS */
+    BIOS_start();
+
+    return (0);
+}
